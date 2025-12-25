2a0cbc57687243c1cddcc29f1472bae231ea98c7
dapao <62489718+qsos@users.noreply.github.com>
2025-12-25 16:27:54 +0800

    Update install-tailscale.sh

--- shell/install-tailscale.sh
+++ shell/install-tailscale.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-# 彻底清理
+# 彻底清理残留
 killall -9 tailscale tailscaled 2>/dev/null
 /etc/init.d/tailscaled stop 2>/dev/null
 rm -rf /etc/tailscale/tailscaled.state
 
-# 安装文件
-mkdir -p /bin /www/cgi-bin /usr/lib/lua/luci/view/tailscale_web
+# 安装文件并强制授权
+mkdir -p /bin /www/cgi-bin /usr/lib/lua/luci/view/tailscale_web /usr/lib/lua/luci/controller
 cp -f bin/tailscale* /bin/
 cp -f www/cgi-bin/tailscale_api /www/cgi-bin/
 cp -f usr/lib/lua/luci/controller/tailscale_web.lua /usr/lib/lua/luci/controller/
 cp -f usr/lib/lua/luci/view/tailscale_web/index.htm /usr/lib/lua/luci/view/tailscale_web/
-chmod 755 /bin/tailscale* /www/cgi-bin/tailscale_api
+chmod -R 755 /bin/tailscale* /www/cgi-bin/tailscale_api /usr/lib/lua/luci/view/tailscale_web
 
-# 启动服务
+# 重新注册服务
 cat << 'EOF' > /etc/init.d/tailscaled
 #!/bin/sh /etc/rc.common
 START=99
